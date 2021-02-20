088980bfd7eec9a126579d0dbc048befff9706a5
kengo-saito <k.saitoh0526@gmail.com>
2021-02-20 23:53:53 +0900

    セグメントを選択すると画像が変更されるように修正

--- MakeupLogger/Views/MakeupLog/MakeupLogViewController.swift
+++ MakeupLogger/Views/MakeupLog/MakeupLogViewController.swift
@@ -10,17 +10,30 @@ import UIKit
 
 final class MakeupLogViewController: UIViewController {
     let segment = UISegmentedControl()
-    let image = UIImageView()
+    
+    let imageWidth = CGFloat(300)
+    let imageHeight = CGFloat(500)
+    
+    lazy var imageCollection: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        layout.itemSize = CGSize(width: imageWidth, height: imageHeight)
+        let collection = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collection.backgroundColor = .red
+        collection.isScrollEnabled = false
+         
+        return collection
+    }()
     
     let viewModel: MakeupLogViewModel
     init(log: MakeupLog) {
         self.viewModel = MakeupLogViewModel(log: log)
         super.init(nibName: nil, bundle: nil)
         
-        viewModel.didChangeSegment = { part in
-            let vm = FacePartViewModel(part: part)
-            let vc = FacePartViewController(viewModel: vm)
-            self.navigationController?.pushViewController(vc, animated: true)
+        viewModel.didChangeSegment = { path in
+            self.imageCollection.selectItem(at: path,
+                                            animated: true,
+                                            scrollPosition: .left)
         }
     }
     
@@ -33,32 +46,30 @@ final class MakeupLogViewController: UIViewController {
         
         view.backgroundColor = .systemGray3
         
-        view.addSubview(image)
-        image.image = viewModel.log.image
-        image.contentMode = .scaleAspectFit
-        
         view.addSubview(segment)
-        
         viewModel.segmentActionList().forEach {
             segment.insertSegment(action: $0.action, at: $0.index, animated: false)
         }
         segment.selectedSegmentIndex = 0
+        
+        view.addSubview(imageCollection)
+        imageCollection.dataSource = viewModel
+        imageCollection.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "id")
     }
     
     override func viewWillLayoutSubviews() {
         super.viewWillLayoutSubviews()
         let mainFrame = view.frame
-        let imageWidth = CGFloat(300)
-        let imageHeight = CGFloat(500)
         let margin = (mainFrame.width - imageWidth)/2
         let safeAreaInsets = view.safeAreaInsets
-        image.frame = CGRect(x: margin,
-                             y: safeAreaInsets.top,
-                             width: imageWidth,
-                             height: imageHeight)
+        
+        imageCollection.frame = CGRect(x: margin,
+                                       y: safeAreaInsets.top,
+                                       width: imageWidth,
+                                       height: imageHeight)
         
         segment.frame = CGRect(x: margin,
-                               y: image.frame.maxY,
+                               y: imageCollection.frame.maxY,
                                width: imageWidth, height: 50)
     }
 }
--- MakeupLogger/Views/MakeupLog/MakeupLogViewModel.swift
+++ MakeupLogger/Views/MakeupLog/MakeupLogViewModel.swift
@@ -8,9 +8,9 @@
 import Foundation
 import UIKit
 
-final class MakeupLogViewModel {
+final class MakeupLogViewModel: NSObject {
     let log: MakeupLog
-    var didChangeSegment: ((FacePart) -> Void)?
+    var didChangeSegment: ((IndexPath) -> Void)?
     
     init(log: MakeupLog) {
         self.log = log
@@ -23,9 +23,12 @@ final class MakeupLogViewModel {
                               attributes: .destructive,
                               state: .on) { _ in
             print("face")
+            self.didChangeSegment?(IndexPath(row: 0,
+                                             section: 0))
         }
         list.append((action: action, index: 0))
         for (index, part) in log.partsList.enumerated() {
+            let indexPlus1 = index + 1
             let action = UIAction(title: part.type,
                                   image: nil,
                                   identifier: nil,
@@ -33,10 +36,48 @@ final class MakeupLogViewModel {
                                   attributes: .destructive,
                                   state: .off) { _ in
                 print(part.type)
-                self.didChangeSegment?(part)
+                self.didChangeSegment?(IndexPath(row: indexPlus1,
+                                                 section: 0))
             }
-            list.append((action: action, index: index + 1))
+            list.append((action: action, index: indexPlus1))
         }
         return list
     }
+}
+
+extension MakeupLogViewModel: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "id",
+                                                            for: indexPath)
+        cell.backgroundColor = .green
+        if indexPath.row == 0 {
+            let image = UIImageView()
+            cell.addSubview(image)
+            image.image = log.image
+            image.frame.size = cell.frame.size
+            image.contentMode = .scaleAspectFit
+            return cell
+        }
+        if indexPath.row == 1 {
+            let part = log.partsList[indexPath.row - 1]
+            let view = AnnotationMoveImageView(image: part.image)
+            view.isUserInteractionEnabled = true
+            view.frame.size = cell.frame.size
+            view.contentMode = .scaleAspectFit
+            part.annotations.forEach {
+                let annotation = AnnotationView(annotation: $0)
+                view.addSubview(annotation)
+            }
+            view.adjustAnnotationViewFrame()
+            cell.contentView.addSubview(view)
+            cell.contentView.isUserInteractionEnabled = true
+            return cell
+        }
+        return cell
+    }
+    
 }
